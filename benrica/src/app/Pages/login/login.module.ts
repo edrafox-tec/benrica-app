59153372127bc00c9d21864ae3dcbfd6dcefb23c
gustavoedrafox <gustavo@edrafox.com>
2023-11-20 17:36:12 -0300

    fingerprint

--- benrica/src/app/Pages/login/login.module.ts
+++ benrica/src/app/Pages/login/login.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginPage } from './login.page';
 
 @NgModule({
@@ -13,8 +14,9 @@ import { LoginPage } from './login.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    SharedModule
   ],
   declarations: [LoginPage]
 })
-export class LoginPageModule {}
+export class LoginPageModule { }
