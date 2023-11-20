ae7049cd78f41d10e0c11230f58ede5bc961c24c
gustavoedrafox <gustavo@edrafox.com>
2023-11-20 15:43:16 -0300

    home page

--- benrica/src/app/Pages/login/login.page.ts
+++ benrica/src/app/Pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BiometryType, NativeBiometric } from "capacitor-native-biometric";
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,51 @@ export class LoginPage implements OnInit {
   ) { }
 
   ngOnInit() {
+
   }
 
   register() {
     this.router.navigate(['/register'])
   }
 
-  login() {
-    this.router.navigate(['/logged/home'])
+  async login() {
+    const result = await NativeBiometric.isAvailable();
+console.log(result);
+
+    if (!result.isAvailable) {
+      this.performRegularLogin();
+      return;
+    }
+    const isFaceID = result.biometryType === BiometryType.FACE_ID;
+    this.performBiometricVerification();
+  }
+
+  private performRegularLogin() {
+    const login = true;
+
+    if (login) {
+      this.router.navigate(['/logged/home']);
+    }
+  }
+
+  private async performBiometricVerification() {
+    const verified = await NativeBiometric.verifyIdentity({
+      reason: "For easy log in",
+      title: "Log in",
+      subtitle: "Maybe add subtitle here?",
+      description: "Maybe a description too?",
+    })
+      .then(() => true)
+      .catch(() => false);
+
+    if (!verified) {
+      this.performRegularLogin();
+      return;
+    }
+    const credentials = await NativeBiometric.getCredentials({
+      server: "www.example.com",
+    });
+    this.router.navigate(['/logged/home']);
   }
 
 }
