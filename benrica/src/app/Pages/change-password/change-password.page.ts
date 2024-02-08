394b90dae444973a16cdc09d09184571b647ea44
gustavoedrafox <gustavo@edrafox.com>
2024-02-08 16:56:00 -0300

    password pages

--- benrica/src/app/Pages/change-password/change-password.page.ts
+++ benrica/src/app/Pages/change-password/change-password.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +10,18 @@ import { Router } from '@angular/router';
 export class ChangePasswordPage implements OnInit {
   public showSpinner = false
 
+
+  public password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  public confirmPassword = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ],
+    [this.validateSamePassword()]);
+
   constructor(
     private router: Router
   ) { }
@@ -18,6 +31,41 @@ export class ChangePasswordPage implements OnInit {
 
   back() {
     this.router.navigate(['logged/settings'])
+  }
+
+  changePassword() {
+
+  }
+
+  validateSamePassword(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      if (this.confirmPassword.value!.length > 0) {
+        const verify = this.password.value === this.confirmPassword.value;
+
+        return new Promise((resolve) => {
+          if (verify) {
+            this.confirmPassword.setErrors({ differentPassword: false });
+            resolve(null);
+          } else {
+            this.confirmPassword.setErrors({ differentPassword: true });
+            resolve({ differentPassword: true });
+          }
+        });
+      } else {
+        this.confirmPassword.setErrors({ differentPassword: false });
+        return Promise.resolve(null);
+      }
+    };
+  }
+
+  validateInputsErros() {
+    if (this.confirmPassword.errors && this.confirmPassword.errors['differentPassword'] !== true) {
+      return false;
+    } else if (this.confirmPassword.errors && this.confirmPassword.errors['differentPassword'] === true) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
--- benrica/src/app/Pages/forget-password/forget-password.page.ts
+++ benrica/src/app/Pages/forget-password/forget-password.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, AsyncValidatorFn, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { userResponseInterface } from 'src/app/models/interfacesResponse';
+import { LoggedService } from 'src/app/services/logged/logged.service';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,17 +10,91 @@ import { Router } from '@angular/router';
   styleUrls: ['./forget-password.page.scss'],
 })
 export class ForgetPasswordPage implements OnInit {
+  public step = 1
   public showSpinner = false
+  public user: userResponseInterface = {
+    id: '',
+    id_businesses: '',
+    user_name: '',
+    email: '',
+    phone_number: '',
+    access_level: '',
+    resset_pass: null,
+    deleted_at: null,
+    created_at: '',
+    updated_at: null
+  }
+
+  public code = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  public password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+
+  public confirmPassword = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ],
+    [this.validateSamePassword()]);
 
   constructor(
-    private router: Router
+    private router: Router,
+    private loggedService: LoggedService,
   ) { }
 
   ngOnInit() {
+    this.user = this.loggedService.getUser()
+    this.step = 1
+  }
+  back() {
+    if (this.step == 1) {
+      this.router.navigate(['logged/settings'])
+    } else {
+      this.step--
+    }
   }
 
-  back() {
-    this.router.navigate(['logged/change-password'])
+  next() {
+    if (this.step == 1) {
+      this.step++
+    } else {
+
+    }
+  }
+
+  validateSamePassword(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      if (this.confirmPassword.value!.length > 0) {
+        const verify = this.password.value === this.confirmPassword.value;
+
+        return new Promise((resolve) => {
+          if (verify) {
+            this.confirmPassword.setErrors({ differentPassword: false });
+            resolve(null);
+          } else {
+            this.confirmPassword.setErrors({ differentPassword: true });
+            resolve({ differentPassword: true });
+          }
+        });
+      } else {
+        this.confirmPassword.setErrors({ differentPassword: false });
+        return Promise.resolve(null);
+      }
+    };
+  }
+
+  validateInputsErros() {
+    if (this.confirmPassword.errors && this.confirmPassword.errors['differentPassword'] !== true) {
+      return false;
+    } else if (this.confirmPassword.errors && this.confirmPassword.errors['differentPassword'] === true) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
